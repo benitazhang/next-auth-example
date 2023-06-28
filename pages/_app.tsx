9c7481f7a5ea8ccb7ce61cb678804b8a5c09e417
Benita <benita@wearethorn.org>
2023-06-28 16:19:41 -0700

    add automatic signIn() call

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,38 @@
-import { SessionProvider } from "next-auth/react"
 import "./styles.css"
+import { useEffect, useState } from "react";
 
 import type { AppProps } from "next/app"
-import type { Session } from "next-auth"
+import { SessionProvider, signIn, useSession } from "next-auth/react";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps<{ session: Session }>) {
+}: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Auth>
+        <Component {...pageProps} />
+      </Auth>
     </SessionProvider>
   )
+}
+
+function Auth({ children }: { children: JSX.Element }) {
+  const { data, status } = useSession();
+  const isUser = !!data?.user;
+
+  useEffect(() => {
+    if (status !== "loading" && !isUser) {
+      signIn("auth0");
+    }
+  }, [isUser, status]);
+
+  if (isUser) {
+    const user = data.user;
+    return children;
+  }
+
+  return <div></div>;
 }
